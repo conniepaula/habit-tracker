1296ccdcaa2df091f502e938b22c4924b002486b
Connie <conniedpaula@gmail.com>
2023-01-20 12:02:22 -0300

    Add brand colour scheme

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#FEFFFA",
+        brand: {
+          100: "#E4F2F1",
+          200: "#BBDEDB",
+          300: "#91C9C4",
+          400: "#6AB4AC",
+          500: "#429488",
+          600: "#3C877B",
+          700: "#34776B",
+          800: "2C685C",
+          900: "#1E4C41",
+        },
+      },
+    },
   },
   plugins: [],
 };
